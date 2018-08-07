060b9bc8e0a6cda58f6b88cdbf64145846f47de0
yungshun317 <seraph317@gmail.com>
2018-08-07 19:59:09 +0800

    Create a Spring Boot project using Spring Initializr via IntelliJ

---
+++ src/main/java/yungshun/chang/springbootlicensingservice/SpringBootLicensingServiceApplication.java
@@ -0,0 +1,12 @@
+package yungshun.chang.springbootlicensingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLicensingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLicensingServiceApplication.class, args);
+    }
+}
